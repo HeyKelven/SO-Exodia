6a4e86828a51ebdfedd99e11b26bb7bd18d14520
HeyKelven <victork.laerte@gmail.com>
2018-08-21 00:54:07 -0300

    Criado lista e usado a função aloca e dealoca

--- malofree.c
+++ malofree.c
@@ -3,6 +3,8 @@
 #include <stdint.h>
 #include <locale.h>
 
+#define malloc aloca
+#define free dealoca
 #define MEMORY_SIZE 4096
 #define LOGAR (long long int)
 
@@ -16,51 +18,44 @@ typedef struct chunck{
 }bloco;
 
 void *aloca(unsigned int);
+void dealoca(void*);
+bloco* createlist(int);
+bloco* insertlastdata(bloco*, int);
+void insertdata(bloco*, int);
+void imprimetudo(bloco*);
+void expulsar(bloco*, int );
 
 int main(void){
-	
+	// ******************* Não apagar *****************
+	setlocale(LC_ALL, "");
 	bloco *voz = (bloco *) &memory[0];
 	voz->size = -1;
-		
-	setlocale(LC_ALL, "");
-	/*
-	bloco *voz, *vo2;
-	voz = (bloco*) &memory[0];
-	vo2 = (bloco*) &memory[sizeof(bloco) - 1 + sizeof(int)];
-	voz->size = sizeof(int);
-	vo2->size = 8;
-	voz->before = NULL;
-	vo2->before = voz;
-	voz->next = vo2;
-	vo2->next = NULL;
-	*/
+	// ****************** Não apagar ******************
 	
-	int * p = (int *) aloca(sizeof(int) + 4000	);
-	*p = 79;
-	printf("%d\n\n", *p);
-	
-	int *w = (int *) aloca(sizeof(int));
-	*w = 2;
-	printf("%d\n\n", *w);
-	
+	bloco *k = createlist(8);
+	insertdata(k, 90);
+	insertdata(k, 13);
+	insertdata(k, 45);
+	insertdata(k, 23);
+	insertdata(k, 54);
+	imprimetudo(k);
+	expulsar(k, 13);
+	imprimetudo(k);
 }
 
-void *aloca(unsigned int rage){
+void* aloca(unsigned int rage){
+	printf("Eu Usei\n");
 	int x = 0;
-	bloco *voz = (bloco *) &memory[x];	
-	/*if (voz->next != NULL){
-		LOGAR voz->next  - ((LOGAR voz) + 23) == voz->size ? printf("Chupa") : printf("Porran"); 
-	}*/
-	
+	bloco* voz = (bloco*) &memory[x];	
+
 	while(1){
 		//Primeiro verifica se existe o espeço solicitado, caso exista segue o programa, se não retorna um erro e exit (1).
-		if(LOGAR &memory[MEMORY_SIZE] - LOGAR voz > rage + 24){
+		if(LOGAR &memory[MEMORY_SIZE] - LOGAR voz > rage + 48){
 				
 			//Verificação se é o primeiro ALOCA solicitado. Retornando assim a posição com o espaço desejado.
 			if (voz->next == NULL && voz->before == NULL && voz->size == -1) {
 				voz->size = rage;
-				printf("Passou Primeiro ponteiro: %d\n", voz->size);
-				return (void *) &memory[x + 24];
+				return (void*) &memory[x + 24];
 				
 			} else {
 				//Verifica se não existe controle para frente, se não tiver cria-se um controle à frente, e depois retorna o primeiro endereço void do espaço.
@@ -74,42 +69,120 @@ void *aloca(unsigned int rage){
 					
 					//x recebe o valor da posição da memória em que o primeiro endereço deve estar
 					x += 24 + voz->size + 24;
-					return (void *) &memory[x];
+					return (void*) &memory[x];
 					
 				} else{
 					if (LOGAR voz->next - LOGAR &memory[x + 24] == voz->size){
 						//Aqui é o caso perfeito, onde a memória foi solicitada uma seguida da outra, então o espaço entre elas será igual ao espaço solicitaado
 						x += 24 + voz->size;
 						voz = voz->next;					
+						
 					}else {
-						if(LOGAR voz->next - LOGAR &memory[x + 24] < rage + 24 + voz->size){
-							//É verificado se existe tamanho suficiente para a memória solicitada
-							//Se não tiver,é passado a "bola" para o próximo bloco de controle e x passa a ter o valor de memory do proximo ponteiro
-							x += LOGAR voz->next - LOGAR &memory[x + 24] ; 
-							voz = voz->next;
-						}else {
-							//caso tenha espaço suficiente entre duas memórias, x toma como número o endereço de memory
-							//após o numero de bytes solicitados pelo bloco controle atual
-							x += 24 + voz->size;
+						if(voz->size == -1){
+							if(LOGAR voz->next - LOGAR &memory[x] < rage + 24){
+								x += LOGAR voz->next - LOGAR &memory[0] ; 
+								voz = voz->next;
+							}else{		
+								voz->size = rage;				
+								x += 24;
+								return (void*) &memory[x];	
+							}
 							
-							bloco *newblock = (bloco *) &memory[x];
-							newblock->size = rage;
-							newblock->next = voz->next;
-							newblock->before = voz;
-							
-							voz->next->before = newblock;
-							voz->next = newblock;
-							
-							x += 24;
-							return (void *) &memory[x];						
+						}else{
+							if(LOGAR voz->next - LOGAR &memory[x + 24] < rage + 24 + voz->size){
+								//É verificado se existe tamanho suficiente para a memória solicitada
+								//Se não tiver,é passado a "bola" para o próximo bloco de controle e x passa a ter o valor de memory do proximo ponteiro
+								x += LOGAR voz->next - LOGAR &memory[0] ; 
+								voz = voz->next;
+								
+								
+							}else {
+								//caso tenha espaço suficiente entre duas memórias, x toma como número o endereço de memory
+								//após o numero de bytes solicitados pelo bloco controle atual
+								x += 24 + voz->size;
+								
+								bloco* newblock = (bloco*) &memory[x];
+								newblock->size = rage;
+								newblock->next = voz->next;
+								newblock->before = voz;
+								
+								voz->next->before = newblock;
+								voz->next = newblock;
+								
+								x += 24;
+								return (void*) &memory[x];						
+							}
 						}
 					}
 				}
 			}
+			
 		}else {
-			printf("\n%d\n", LOGAR &memory[MEMORY_SIZE] - LOGAR voz);
+			//Cado o espaço seja insuficiente, retorna um erro.
 			fprintf(stderr, "%s", "Não há espaço!");
 			exit(1);
 		}
 	}	
+}
+
+void dealoca(void* freedom){
+	printf("Eu usei aqui oh\n");
+	int x = 0;
+	while(freedom != &memory[x]) x++;
+	
+	bloco* voz;
+	voz = &memory[x - 24];
+	
+	voz->before != NULL ? (voz->before->next = voz->next) : (voz->before = NULL);
+	voz->next != NULL ? (voz->next->before = voz->before): (voz->next = NULL);
+
+	voz->before = NULL;
+	voz->size = -1;
+}
+
+bloco* createlist(int x){
+	
+	//Create a new list
+	bloco* newblock = (bloco*) malloc(sizeof(bloco));
+	newblock->next = NULL;
+	newblock->before = NULL;
+	newblock->size = x;
+	return newblock;
+}
+
+bloco* insertlastdata(bloco *last, int x){
+	
+	bloco* newblock = (bloco *) malloc(sizeof(bloco));
+	newblock->next = NULL;
+	newblock->before = last;
+	newblock->size = x;
+	return newblock;
+}
+
+void insertdata(bloco* voz, int x){
+	while(voz->next != NULL){
+		voz = voz->next;
+	}
+	voz->next = insertlastdata(voz, x);	
+}
+
+void imprimetudo(bloco* voz){
+	while(voz != NULL){
+		printf("Valor do item: %d\n", voz->size);
+		voz = voz->next;
+	}
+}
+
+void expulsar(bloco* voz, int x){
+	while(voz->size != x){
+		if(voz== NULL)
+			printf("Não existe o valor\n");
+		else
+			voz = voz->next;
+	}
+	
+	voz->before->next = voz->next;
+	voz->next->before = voz->before;
+	
+	free(voz);
 }
